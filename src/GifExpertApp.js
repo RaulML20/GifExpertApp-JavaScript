98b3877521c79f1156f63c81f33874b54bd3069c
RaulML20 <meizosolopezraul@gmail.com>
2022-06-20 14:50:24 +0200

    Button and input added

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,12 +1,30 @@
-import {Box, Grid} from '@material-ui/core';
+import { useState } from "react";
 
 function GifExpertApp(){
+
+    const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball' ])
+    const [inputValue, setInputValue] = useState([ 'New Category' ])
+
+    const addCategory = () => {
+        setCategories([...categories, inputValue]);
+    }
+
+    const onInputChange = (event) => {
+        setInputValue( event.target.value );
+    }
+
     return(
         <div className="GifExpertApp">
-            <Grid container className={GifExpertApp} justifyContent="center" alignItems="center" spacing={4}>
-                <h1>This is the title</h1>
-                <h2>This is a subtitle</h2>
-            </Grid>
+            <h1>This is the title</h1>
+            <h2>This is a subtitle</h2>
+            <input type="text" value={ inputValue } onChange = { (event) => onInputChange(event) }>
+            </input>
+            <ol>
+                { categories.map(category => {
+                    return <li key={ category }>{category}</li>
+                })}
+            </ol>
+            <button onClick={ addCategory }>Click here to add a new category</button>
         </div>
     );
 }
